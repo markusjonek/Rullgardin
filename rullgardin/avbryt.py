4ee19302806303cb234651cfece6b4336e546b3b
markusjonek <markus.jonek@gmail.com>
2022-03-12 17:15:13 +0100

    delete

--- rullgardin/avbryt.py
+++
@@ -1,8 +0,0 @@
-from rullgardin import Rullgardin
-import os
-
-Rullgardin(4, 22, 17, 26).off()
-
-os.system("pkill -9 -f klocka.py")
-os.system("pkill -9 -f ner.py")
-os.system("pkill -9 -f upp.py")
--- rullgardin/reset_logg.py
+++
@@ -1,4 +0,0 @@
-open('/home/pi/Programs/rullgardin/logg1.txt', 'w').close()
-with open('/home/pi/Programs/rullgardin/logg1.txt', 'w') as f:
-    f.write('0')
-    f.write('\n')

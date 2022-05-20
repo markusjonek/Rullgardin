432616e36cb701b9cffbb27ffb9d97c9541e5554
Markus jonek <markus.jonek@gmail.com>
2022-05-20 09:46:59 +0200

    städa

--- rullgardin.py
+++ rullgardin.py
@@ -28,54 +28,8 @@ class Rullgardin:
         self.a1.on()
         self.en.on()
 
-    def read_log(self):
-        fil = open(logg_path + "logg" + self.index + ".txt", encoding="utf8")
-        fil_rader = fil.readlines()
-        for index, rad in enumerate(fil_rader):
-            fil_rader[index] = rad.rstrip("\n")
-        if len(fil_rader) < 1:
-            print("error i logging för gardin: " + self.index)
-            return 0
-        return float(fil_rader[-1])
-
-    def log(self, tid):
-        with open(logg_path + "logg" + self.index + ".txt", "a") as f:
-            f.write(tid)
-            f.write("\n")
-
-    def clear_log(self):
-        open(logg_path + "logg" + self.index + ".txt", "w").close()
-
-    def ner_logger(self):
-        gammal_tid = self.read_log()
-        start = time()
-        while True:
-            try:
-                self.log(str(time() - start))
-                sleep(0.1)
-                if time() - start + gammal_tid > self.tid:
-                    self.clear_log()
-                    self.log(str(self.tid))
-                    break
-            except KeyboardInterrupt:
-                print("Avbryter")
-                break
-
-    def upp_logger(self):
-        gammal_tid = self.read_log()
-        start = time()
-        while self.knapp.value != 1:
-            self.log(str(gammal_tid - (time() - start) * 0.87))
-            if time() - start > 24:
-                self.off()
-                self.clear_log()
-                self.log("0")
-                break
-        self.clear_log()
-        self.log("0")
-
 def gardiner():
-    file = open(logg_path + "gardiner.txt", encoding="utf8")
+    file = open("/home/pi/Rullgardin/gardiner.txt", encoding="utf8")
     file_rader = file.readlines()
     rullgardiner = []
     for rad in file_rader:
